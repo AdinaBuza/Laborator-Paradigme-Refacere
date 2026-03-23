50aa269572724477bbdd6074249a10eea53ac523
AdinaBuza <adinabuza15@gmail.com>
2026-03-23 17:38:34 +0200

    3.5.1

---
+++ src/lab3/Main.java
@@ -0,0 +1,34 @@
+package lab3;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ProcesareText procesor = new ProcesareText();
+
+        try {
+            List<String> liniiInitiale = procesor.citesteLinii("in.txt");
+
+            List<String> rezultatA = procesor.adaugaRandExtra(liniiInitiale);
+            System.out.println("Subpunctul A procesat ");
+
+            List<String> rezultatB = procesor.adaugaRandDupaPunct(liniiInitiale);
+            System.out.println("Subpunctul B procesat ");
+
+            List<String> finalDeSalvat = new ArrayList<>();
+            finalDeSalvat.add("REZULTAT A ");
+            finalDeSalvat.addAll(rezultatA);
+            finalDeSalvat.add("\n REZULTAT B ");
+            finalDeSalvat.addAll(rezultatB);
+
+            procesor.salveazaInFisier("out.txt", finalDeSalvat);
+
+            System.out.println("\nSucces! Fișierul out.txt a fost generat.");
+
+        } catch (IOException e) {
+            System.out.println("Eroare la citirea sau scrierea fișierelor: " + e.getMessage());
+        }
+    }
+}
---
+++ src/lab3/ProcesareText.java
@@ -0,0 +1,35 @@
+package lab3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcesareText {
+
+    public List<String> citesteLinii(String numeFisier) throws IOException {
+        String continut = Files.readString(Paths.get(numeFisier));
+        String[] liniiArray = continut.split("\\r?\\n");
+        return new ArrayList<>(List.of(liniiArray));
+    }
+    public List<String> adaugaRandExtra(List<String> linii) {
+        List<String> rezultat = new ArrayList<>();
+        for (String linie : linii) {
+            rezultat.add(linie + "\n");
+        }
+        return rezultat;
+    }
+
+    public List<String> adaugaRandDupaPunct(List<String> linii) {
+        List<String> rezultat = new ArrayList<>();
+        for (String linie : linii) {
+            rezultat.add(linie.replace(".", ".\n"));
+        }
+        return rezultat;
+    }
+
+    public void salveazaInFisier(String numeFisier, List<String> date) throws IOException {
+        Files.write(Paths.get(numeFisier), date);
+    }
+}
